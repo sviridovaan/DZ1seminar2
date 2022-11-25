a2a6afe6c30383d6e8778433ee68db991b3efcd4
Anna <sviridova_anya@mail.ru>
2022-11-25 23:02:09 +0300

    Console.Clear();

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿//напишите програму, которая принимает на вход трехзначное число и на выходе показывает вторуб цифру числа
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int ostatok = number/10%10;
+Console.WriteLine($"Вторая цифра числа {ostatok}");
